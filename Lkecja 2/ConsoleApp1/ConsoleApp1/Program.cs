4e0f14c829b16d11e6a5c2ee188594866829d0ec
Kamil6747 <kamlbaron3523@gmail.com>
2026-09-17 15:19:08 +0200

    Rzut moneta

---
+++ Lkecja 2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Diagnostics;
+
+Console.WriteLine("Hello, World!");
+/*
+int  b ;
+
+//DEBUGGER
+for(int i = 0; i < 10; i++)
+{
+    b = i;
+    Debug.WriteLine(b);
+
+}
+
+//Debugger z Breakpointem 
+
+for (int i = 0; i < 10; i++)
+{
+    b = i;
+
+}
+
+//Breakpoint when changed
+
+for (int i = 0; i < 10; i++)
+{
+    b = i;
+
+}
+//Breakpoint one hit
+int x = 0;
+for (int i = 0; i < 10; i++)
+{
+    x++;
+    if (x == 6)
+    {
+        x = 0;
+    }
+    b = i;
+
+}
+
+//F10 - STEP OVER bez wchodzenia do funkcji/metody
+//F11 - STEP INTO wejdz do funkcji/metody
+//SHIFT + F11 - STEP OUT wyjdź z aktualnej metody
+/*while (true)
+{
+    int a = 0;
+    int c = 0;
+
+    a++;
+    c++;
+
+    Console.WriteLine(DodajDwieLiczby(a, c));
+
+};
+
+int DodajDwieLiczby(int a, int b)
+{
+    return (a + b);
+}
+
+
+*/
+
+
+int[] tablica = new int[11];
+
+for(int i = 0; i <= 10; i++)
+{
+    tablica[i] = i;
+}
---
+++ Lkecja 3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,82 @@
+﻿
+
+
+
+
+
+
+
+
+void Restert(ref int x) {
+    int Count = 0;
+    int max;
+    Boolean Ready = false;
+   
+    for (int i = 0; Ready == false; Count++)
+    {
+
+        int Lotto = new Random().Next(1, 3);
+        max = Lotto ;
+
+        Console.WriteLine(Lotto);
+
+        if (max == 1)
+        {
+
+            Ready = true;
+        }
+       
+    }
+    
+
+    x = Count;
+}
+;
+int rekord = 0;
+int x = 0;
+bool y = false;
+while (true)
+{
+    
+   
+    Console.ReadKey();
+    y = false;
+    for(int t = 0; y ==false; t++) { 
+    Console.Clear();
+    
+    Restert(ref x);
+    
+    if(rekord < x)
+    {
+        rekord = x;
+        Console.Beep(1000, 10);
+            y = true;
+    }
+    
+    }
+    Console.WriteLine(rekord);
+
+}   
+
+/*for (int i = 0; Ready == false; Count++)
+{
+    int Lotto = new Random().Next(0, 10);
+    tablica[Lotto] = Lotto;
+    if (tablica[0] == 0 && tablica[1] == 1 && tablica[2] == 2 && tablica[3] == 3 && tablica[4] == 4 && tablica[5] == 5 && tablica[6] == 6 && tablica[7] == 7 && tablica[8] == 8 && tablica[9] == 9)
+    {
+
+
+        Ready = true;
+        Console.Beep(1000, 1000);
+    }
+}
+
+    for (int i = 0; i < tablica.Length; i++)
+{
+    Console.WriteLine(tablica[i]);
+    
+}*/
+
+
+
+
